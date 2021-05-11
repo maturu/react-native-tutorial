1c5a2f0d5306df5cdb5b174abba8d61525dfe490
maturu <maturu.410@gmail.com>
2021-05-12 00:55:51 +0900

    React Navigation Tutorial: Getting started & Hello React Navigation & Passing parameters to routes 3/6

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
+  const item = {
+    id: 86,
+    otherParam: 'anything you want here',
+  };
+
+  return (
+    <View style={styles.container} options={{ title: 'Overview' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', item);
+        }}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ route, navigation }) {
+  /* Get the param */
+  const params = route.params;
+  const randomId = Math.floor(Math.random() * 100);
+
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(params.id)}</Text>
+      <Text>otherParam: {JSON.stringify(params.otherParam)}</Text>
+      <Button
+        title="Change params"
+        onPress={() => navigation.setParams({ id: randomId })}
+      />
+      <Button
+        title="Go to Details...again"
+        onPress={() => {
+          navigation.push('Details', { ...params, id: randomId });
+        }}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
